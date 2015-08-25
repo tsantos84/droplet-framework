650fb9ff331a9a77486353b1c22379be0890348b
Tales Santos <tales.augusto.santos@gmail.com>
2015-08-25 17:39:12 -0300

    Removing the doctrine droplet
    This droplet will have its own git repository and will add to application by external dependency

--- test/app/MyApplication.php
+++ test/app/MyApplication.php
@@ -2,7 +2,6 @@
 
 use Framework\Application;
 use Framework\Droplet\Core\CoreDroplet;
-use Framework\Droplet\Extension\DoctrineDroplet;
 
 /**
  * Class MyApplication
@@ -15,6 +14,5 @@ class MyApplication extends Application
     public function registerDroplets()
     {
         $this->registerDroplet(new CoreDroplet());
-        $this->registerDroplet(new DoctrineDroplet());
     }
 }
