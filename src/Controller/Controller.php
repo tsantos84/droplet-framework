15fbef42b82070b570524126e712ee18d8258abf
Tales Santos <tales.augusto.santos@gmail.com>
2015-08-25 18:12:59 -0300

    Optional base controller
    This controller will have some shortcuts for common tasks on controllers

---
+++ src/Controller/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Framework\Controller;
+
+use Framework\DependencyInjection\AbstractContainerAware;
+
+/**
+ * Class Controller
+ * @package Framework\Controller
+ */
+class Controller extends AbstractContainerAware
+{
+
+}
