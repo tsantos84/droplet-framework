c3a2a15b96ed4de99133c45db19996e65ef35a6b
Tales Santos <tales.augusto.santos@gmail.com>
2015-08-25 23:20:04 -0300

    Kernel services moved to its own droplet

--- test/app/MyApplication.php
+++ test/app/MyApplication.php
@@ -1,6 +1,7 @@
 <?php
 
 use Framework\Application;
+use Framework\Droplet\Core\KernelDroplet;
 use Framework\Droplet\Core\ParameterDroplet;
 use Framework\Droplet\Core\RoutingDroplet;
 use Framework\Droplet\Core\TemplatingDroplet;
@@ -15,6 +16,7 @@ class MyApplication extends Application
      */
     public function registerDroplets()
     {
+        $this->registerDroplet(new KernelDroplet());
         $this->registerDroplet(new ParameterDroplet());
         $this->registerDroplet(new RoutingDroplet());
         $this->registerDroplet(new TemplatingDroplet());
